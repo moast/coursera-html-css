872efd72f8bb83f2c87d92f1c8a0ab1438f9ea5f
moast <unv2000@hotmail.com>
2016-11-08 14:20:22 -0800

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
                                  categoriesTitleHtml,
                                  categoryHtml)
 
-        insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
+        insertHtml("#main-content",chosenCategoryShortName);
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
